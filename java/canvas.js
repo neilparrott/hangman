a5d4062d807f2f22145b2ce919278df3d707c5e0
neilparrott <77660430+neilparrott@users.noreply.github.com>
2024-04-17 15:47:12 +0100

    Add files via upload

---
+++ java/canvas.js
@@ -0,0 +1,79 @@
+function draw(part) {
+  ctx.strokeStyle = "white";
+  ctx.lineWidth=5;
+  ctx.beginPath();
+  switch(part) {
+    case 1:
+      // base
+      ctx.moveTo(210,405);
+      ctx.lineTo(400,405);
+      ctx.stroke();
+      break;
+    case 2:
+      // pole
+      ctx.beginPath();
+      ctx.moveTo(225,405);
+      ctx.lineTo(225,80);
+      ctx.stroke();
+      break;
+    case 3:
+      // top pole
+      ctx.beginPath();
+      ctx.moveTo(225,80);
+      ctx.lineTo(340,80);
+      ctx.stroke();
+      break;  
+    case 4:
+      // rope
+      ctx.beginPath();
+      ctx.moveTo(325,80)
+      ctx.lineTo(325,120);
+      ctx.stroke();  
+      break;
+    case 5:
+      // head
+      
+      ctx.beginPath();
+      
+      ctx.arc(325, 140, 20, 0, Math.PI * 2); 
+      ctx.stroke(); 
+     
+      break;
+    case 6:
+      // body
+      ctx.beginPath();
+      ctx.rect(320, 164, 10, 80);
+      ctx.stroke();
+      break;
+    case 7:
+      // arms
+      ctx.beginPath();
+      ctx.moveTo(320, 164);
+      ctx.lineTo(300, 200);
+      ctx.lineTo(270,220);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(328, 164);
+      ctx.lineTo(356, 200);
+      ctx.lineTo(385,220);
+      ctx.stroke();
+      break;
+    case 8:
+      // legs
+      ctx.beginPath();
+      ctx.moveTo(320, 246);
+      ctx.lineTo(300, 300);
+      ctx.lineTo(310,340);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(330,246);
+      ctx.lineTo(350,300);
+      ctx.lineTo(370,275);
+      ctx.stroke();
+      msg(`Sorry, you lost`,50,225);
+      msg(`${chosenWord}`,50,275);
+      break;
+    default:
+        // code block
+  }
+}
---
+++ java/script.js
@@ -0,0 +1,230 @@
+
+const longwords = [
+  "protagonist","antagonist","prosthetic", "promontory",
+  "elementary","dystopian","utilitarian","libertarian",
+  "consequential","transportation"
+];
+const countries = [
+  "england","australia","india","russia",
+  "new zealand","china",
+  "afghanistan","papua new guinea",
+
+  "columbia","venezuela","croatia","bolivia","chile","venezuela","ecuador","guyana",
+  "peru","chile","argentina","uruguay","paraguay","bolivia","brazil","suriname",
+  "french guinea","aruba","trinidad and tobago","falkland islands","curacao",
+  "panama","nicaragua",
+
+  "egypt","saudi arabia","yemen","tunisia","south africa","nigeria","kenya",
+  "morocco","ghana","zambia","gambia","senegal","south sudan","tanzania","ethiopia",
+  "north sudan","ivory coast","algeria","niger","libya","lebanon","uganda","cameroon",
+  "rwanda","mali","madagascar","cote divoire","somalia","angola","zimbabwe","cabo verde",
+  "burkina faso","namibia","chad","guinea","mozambique","burundi","zambia","togo","botswana",
+  "sierra leone","liberia","gabon","mauritania","central african republic","benin",
+  "equatorial guinea","malawi","mauritius","eritrea","djibouti","republic of congo",
+  "western sahara","seychelles","lesotho","eswatini",
+
+  
+  "italy","spain","portugal","romania","bulgaria","poland","france",
+  "hungary","albania","norway","sweden","denmark","finland","austria","germany",
+  "latvia","estonia","georgia","ukraine","turkey","greece","belgium","czech republic",
+  "ireland","croatia","malta","luxembourg","netherlands","slovenia",
+
+  "iran","iraq","israel",
+  "pakistan","bangladesh","turkmenistan","azerbaijam",
+  "japan","north korea","south korea","phillipines","thailand",
+  "malaysia","singapore"
+];
+const dogbreeds = [
+  "mauser","labrador","poodle","crossbreed","alsation",
+  "boxer","pug","dalmation","cocker spaniel","chihuahua",
+  "dachsund","red setter","golden retriever","king charles spaniel",
+  "greyhound","whippet","french bulldog","wire terrier",
+  "bearded terrier","old english sheepdog","south african ridgeback",
+  "corgi","border collie", "beagle","rottweiler","dobermann",
+  "sheltie","samoyed","shih tzu","siberian husky",
+  "yorkshire terrier"
+];
+const shortwords = [
+  "and","who","the","what","open","able","why","paid","lid",
+  "rot","rat","pot","pose","post","host","town","zoo",
+  "zoom"
+];
+const horsebreeds = [
+"Akhal-Teke","American Quarter horse","American Saddle horse","Andalusian",
+"Anglo-Arab","Anglo-Norman","Appaloosa","Arab","Ardennes","Balearic","Barb",
+"Basuto","Batak or Deli","boerperd","Beetewk","Brabançon","Breton","BurmeseS",
+"Cleveland Bay","Clydesdale","Connemara","Criollo","Dales pony","Danish","Dartmoor pony",
+"Don","Dutch Draught","Esthonian","Exmoor","Fell pony","Finnish horse","Fjord pony",
+"Flemish","Friesian","Gelderland","Gidran","Groningen","Gudbrandsdal","Hackney",
+"Hafflinger","Hambletonian","Hanoverian","Highland pony","Huçul","Iceland pony",
+"Iomud","Kabarda","Karabair","Karabakh","Karadagh","Kathiawari","Kladruber","Klepper",
+"Knabstrup","Konik","Kurdistan pony","Limousin","Lipizzaner","Lokai","Manipur",
+"Marwari","Mecklenburg","Mongolian","Morgan","mustang","New Forest pony","Nonius",
+"North Swedish horse","Oldenburg","Orlov Trotter","Palomino","Percheron","Persian Arab",
+"Pinto","Pinzgauer","Polish Arab","Polish Half-bred","Polish Thoroughbred","Quarter horse",
+"racehorse","Rhenish","Shagya","Shetland pony","Shirazi","Shire horse","Spanish Jennet", 
+"Spiti","Standard Bred","Strelet","Suffolk Punch","Swedish Ardennes","Tarbenian","Tarpan",
+"Tennessee Walking Horse","Thoroughbred","Timor pony","Trakehner","Turk","Viatka","Waler",
+"Welsh Cob","Welsh Mountain pony","Welsh pony","Yamoote","Yorkshire Coach horse",
+"Zeeland horse","Zemaitukas"
+];
+let correct = 0; // count correct letters so we can test for WIN
+let part = 1;    // initialise part to draw first
+let chosenWord = "";
+let x = [];
+let newArray = [];
+let selected = "";
+//getOption();     // get the word topic
+
+const canvasElement = document.querySelector(".myCanvas");
+canvasElement.height=450;
+canvasElement.width=450;
+const ctx = canvasElement.getContext("2d");
+let areEventListenersAssigned = 0;
+
+function begin() {
+  getOption();     // get the word topic
+  //console.log(newArray);
+  chosenWord = newArray[pickWord(newArray.length)];
+  reset();
+  drawUnderlines(chosenWord.length);
+  if (areEventListenersAssigned === 0) {
+    x = document.querySelectorAll(".letter > *");
+    x.forEach(item => {
+      item.addEventListener('click', () => {
+        process(`${item.innerText}`,chosenWord.toUpperCase(),item);
+      });
+    });
+    areEventListenersAssigned = 1;
+  };
+}
+
+function pickWord(max) {
+  return (Math.floor(Math.random() * (max - 1 +1)) + 1) - 1; 
+};
+
+function process(letter,chosenWord, node) {
+  /* is letter in chosenWord */
+  /* hide button with letter selected */
+  node.disabled = true;
+  let letterl = letter.toUpperCase();
+  console.log("chosenword: " + chosenWord + " letter: " + letterl );
+  if (chosenWord.includes(`${letterl}`)) {
+    drawLetter(letter);
+  } else {
+    draw(part);
+    part++;
+  }
+
+};
+
+function reset() {
+    /* get new chosen word */
+  correct = 0;      // reset correct letter count  
+  part = 1;         // reset part to draw  
+  chosenWord = newArray[pickWord(newArray.length)];
+
+    /* undisable all nodes for the letters */
+  x.forEach(item => {
+    item.disabled= false ;
+  });
+  drawUnderlines(chosenWord.length);
+};
+
+function drawUnderlines(len) {
+  
+  ctx.clearRect(0,0,canvasElement.height,canvasElement.width); 
+
+  ctx.strokeStyle = "white";
+  ctx.fillStyle = "white";
+  ctx.textAllign= "right";
+  ctx.lineWidth=2;
+  ctx.font = "30px Arial";
+  
+  for (let i = 1; i <= len; i++) {
+    if (chosenWord.charAt(i-1) != " ") {
+      ctx.beginPath();
+      ctx.moveTo(i * 18,50);
+      //ctx.fillText(chosenWord.charAt(i-1),i*20,50);
+      ctx.lineTo(i * 18 + 13,50);
+      ctx.stroke();
+    };
+  }; 
+};
+
+function drawLetter(letter) {
+  //console.log(`drawletter: ${letter}  pre correct: ${correct}`);
+  let positions = [];  // array to hold positions
+  for (let i = 0; i < chosenWord.length; i++) {
+    if (chosenWord.charAt(i) != " ") {
+      if (chosenWord.charAt(i) === letter.toLowerCase()) {
+        positions.push(1);   // position in string 
+        correct++;
+      } else {
+        positions.push(0);
+      }
+    } else {
+        positions.push(0);
+    };
+  }
+  //console.log(`drawletter: ${letter}  post correct: ${correct}`);
+  ctx.strokeStyle = "white";
+  ctx.fillStyle = "white";
+  //console.log(positions);
+  for (let i = 0; i < chosenWord.length; i++) {
+    ctx.beginPath();
+    if ( positions[i] === 1) {
+      ctx.fillText(letter,(i+1)*18,50);
+      ctx.stroke();
+    } 
+  } 
+  if (correct + countBlanks() === chosenWord.length) {
+    msg("Congratulations!!",40,225);
+  }
+}
+function msg(message,len,y) {
+  ctx.clearRect(0,0,canvasElement.height,canvasElement.width);
+  setTimeout( () => {
+    ctx.strokeStyle = "yellow";
+    ctx.fillStyle = "red";
+    ctx.font = `${len}px Arial`;
+    ctx.beginPath();
+    ctx.fillText(message,40,y);
+    ctx.stroke();
+  },2000);
+  setTimeout( () => {
+    reset();
+  },4000); 
+}
+function getOption() {
+  var selectBox = 
+    document.getElementById('word-topic');
+  var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+  //console.log(selectedValue) ;
+  selected = selectedValue;
+  //document.querySelector(".output").textContent = selected;
+
+  //console.log(selected);
+  if (selected === "countries") {
+    newArray = countries;
+  } else if (selected === "horse-breeds") {
+    newArray = horsebreeds;  
+  } else if (selected === "dog-breeds") {
+    newArray = dogbreeds;
+  } else if (selected === "long-words") {
+    newArray = longwords;
+  } else if (selected === "short-words") {
+    newArray = shortwords;
+  };
+
+  return selected;
+}
+function countBlanks() {
+  let noBlanks = 0;
+  for (let i=1; i < chosenWord.length; i++) {
+    if (chosenWord.charAt(i-1) === " ") {
+      noBlanks++;
+    }
+  }
+  return noBlanks;
+}; 
